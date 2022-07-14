a6a5f3f936deec0f97e17352aa8c8a80642da53d
Gloria Li <geling.li@microsoft.com>
2022-07-14 00:14:24 -0700

    Add project files.

---
+++ CpmDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CpmDemoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
