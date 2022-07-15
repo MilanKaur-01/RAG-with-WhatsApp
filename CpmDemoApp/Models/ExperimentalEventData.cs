015c7177e5f381049b8c9a7f80266eb57fed5ad6
Gloria Li <geling.li@microsoft.com>
2022-07-14 18:15:12 -0700

    renaming file to ExperimentalEventData

--- CpmDemoApp/Models/ExperimentalEventData.cs
+++ CpmDemoApp/Models/ExperimentalEventData.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace CpmDemoApp.Models
 {
     public class ExperimentalEventData
@@ -17,18 +15,4 @@ namespace CpmDemoApp.Models
         public string ExternalMessageId { get; set; }
         public string ReceivedTimeStamp { get; set; }
     }
-
-    public class ExternalMessageEvent
-    {
-        public string Id { get; set; }
-        public string Topic { get; set; }
-        public string Subject { get; set; }
-        public BinaryData Data { get; set; }
-        public string EventType { get; set; }
-        public string DataVersion { get; set; }
-        public string MetadataVersion { get; set; }
-        public DateTime EventTime { get; set; }
-    }
-
-
 }
